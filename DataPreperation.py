5010a44218224730f4e6a0ea6d077aa98eacaf62
satlawa <p.satlawa@gmx.net>
2021-03-27 17:45:05 +0100

    bug fix

--- DataPreperation.py
+++ DataPreperation.py
@@ -13,7 +13,7 @@ class DataPreperation(object):
             self.path_out_dir = path_dir + '/'
 
 
-    def clip(self, path_in_grid, path_in_raster, path_out_dir, typ='ortho'):
+    def clip_by_grid(self, path_in_grid, path_in_raster, path_out_dir, data_type='ortho'):
         '''
         clip raster by grid
 
@@ -21,13 +21,13 @@ class DataPreperation(object):
             path_in_grid (string) - path to vector (.shp) file containing polygons
             path_in_raster (string) - path to raster (.tif) that needs to be cutted
             path_out_dir (string) -
-            typ (string) - name of type (exp. 'ortho', 'dsm', 'dtm', 'slope')
+            data_type (string) - name of type (exp. 'ortho', 'dsm', 'dtm', 'slope')
         '''
 
         # load vector file
         grid = geopandas.read_file(path_in_grid)
 
-        if typ == 'ortho':
+        if data_type == 'ortho':
             sufix = ''
         else:
             sufix = '_1m'
@@ -45,7 +45,7 @@ class DataPreperation(object):
             # get extend
             extend = grid.loc[i,'geometry'].bounds
             # set paths
-            path_out = "{}{}{}/tile_{}_{}.tif".format(path_out_dir, typ, sufix, typ, grid.loc[i,'id'])
+            path_out = "{}{}{}/tile_{}_{}.tif".format(path_out_dir, data_type, sufix, data_type, grid.loc[i,'id'])
 
             # construct bash command
             bash_command = "gdal_translate -projwin {} {} {} {} -a_nodata 0.0 -of GTiff {} {}".format(extend[0], extend[3], extend[2], extend[1], path_in_raster, path_out)
@@ -63,27 +63,27 @@ class DataPreperation(object):
             path_dir (string) - path to directory
 
         returns:
-            ids (list) - list with all file paths in the directory
+            file_names (list) - list with all file paths in the directory
         '''
-        ids = []
+        file_names = []
         # loop over all files found in directory
         for file in os.listdir(path_dir):
             # add path to lists
-            ids.append(path_dir + file)
-        return ids
+            file_names.append(path_dir + file)
+        return file_names
 
 
-    def align_tiles(self, path_dir, typ):
+    def align_tiles(self, path_dir, data_type):
         '''
         converts the resolution of an tif image into the desired resolution
 
         input:
             path_dir (string) - path to directory
-            typ (string) - type of (exp: 'dsm')
+            data_type (string) - type of (exp: 'dsm')
 
         '''
-        path_dir_in = "{}{}_1m/".format(path_dir, typ)
-        path_dir_out = "{}{}/".format(path_dir, typ)
+        path_dir_in = "{}{}_1m/".format(path_dir, data_type)
+        path_dir_out = "{}{}/".format(path_dir, data_type)
 
         file_paths = self.get_file_paths(path_dir_in)
 
@@ -102,12 +102,14 @@ class DataPreperation(object):
 
                 #set file paths
                 path_in = file_path
-                path_out = '{}tile_{}'.format(path_dir_out, file_path[file_path.rfind(typ):])
+                path_out = '{}tile_{}'.format(path_dir_out, file_path[file_path.rfind(data_type):])
 
                 # construct bash command
                 bash_command = "gdalwarp -tr 0.2 0.2 -tap " + path_in + " " + path_out
                 # execute bash command
                 os.system(bash_command)
+
+        print("finished aligning")
 
 
     def create_slope(self, input_path, output_path):
@@ -118,7 +120,7 @@ class DataPreperation(object):
             input_path (string) - path to the DTM raster
             output_path (string) - path to the slope raster to be computed
         '''
-        if self.dtm_path is not None:
+        if input_path is not None:
             # create string containing bash command
             cmd = "gdaldem slope {} {} -of GTiff".format(input_path, output_path)
 
