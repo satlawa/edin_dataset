fixed bug in std calculation

DatasetCreation.py
@@ -307,45 +307,70 @@ class DatasetStats(object):
             self.path_dir = path_dir + '/'
 
 
-    def calc_mean(self, dset, data_type, start_index, end_index, block_size=10000, band=0):
+    def calc_mean(self, dset, data_type, start_index, end_index, block_size=10000, bands=1):
 
-        ds_size = end_index - start_index
+        mean_bands = list()
+        for band in range(bands):
 
-        means = []
+            ds_size = end_index - start_index
 
-        counter = start_index
-        for i in range(ds_size // block_size):
-            # calc start and end index
-            start = counter + block_size*i
-            end = counter + block_size*(i+1)
-            # append the mean
-            means.append(np.moveaxis(dset[data_type][start:end], 3, 0)[band].mean())
-        # calc start index, end index and rest
-        rest = ds_size % block_size
-        start = counter + block_size*(i+1)
-        end = end_index
-        # append the mean multiplied with the percentage (rest/block_size)
-        means.append(np.moveaxis(dset[data_type][start:end], 3, 0)[band].mean() * (rest/block_size))
-        # calc the mean
-        mean = sum(means) / (len(means)-1+(rest/block_size))
+            means = []
 
-        return mean
+            counter = start_index
+            for i in range(ds_size // block_size):
+                # calc start and end index
+                start = counter + block_size*i
+                end = counter + block_size*(i+1)
+                # append the mean
+                means.append(np.moveaxis(dset[data_type][start:end], 3, 0)[band].mean())
+            # calc start index, end index and rest
+            rest = ds_size % block_size
+            start = counter + block_size*(i+1)
+            end = end_index
+            # append the mean multiplied with the percentage (rest/block_size)
+            means.append(np.moveaxis(dset[data_type][start:end], 3, 0)[band].mean() * (rest/block_size))
+            # calc the mean
+            mean_bands.append(sum(means) / (len(means)-1+(rest/block_size)))
+
+        return mean_bands
 
 
-    def calc_std(self, mean, dset, data_type, start, end, block_size=10000, band=0, scaler=1):
+    def calc_std(self, mean, dset, data_type, start, end, block_size=10000, bands=1, scaler=1):
 
         #ds_size = end_index - start_index
+        if bands > 1:
 
-        # sum squared difference
-        sum_sqr = list()
-        for i in range(end//block_size):
-            sum_sqr.append(((np.moveaxis(dset[data_type][start+(i)*block_size:start+(i+1)*block_size], 3, 0)[band]/scaler - (mean/scaler))**2).sum())
-        sum_sqr.append(((np.moveaxis(dset[data_type][start+(i+1)*block_size:end], 3, 0)[band]/scaler - (mean/scaler))**2).sum())
+            std_bands = list()
+            for band in range(bands):
+                print('Band: {}'.format(band))
+                # sum squared difference
+                sum_sqr = list()
+                for i in range(end//block_size):
+                    sum_sqr.append(((np.moveaxis(dset[data_type][start+(i)*block_size:start+(i+1)*block_size], 3, 0)[band]/scaler - (mean[band]/scaler))**2).sum())
+                sum_sqr.append(((np.moveaxis(dset[data_type][start+(i+1)*block_size:end], 3, 0)[band]/scaler - (mean[band]/scaler))**2).sum())
 
-        # count records
-        sum_n = list()
-        for i in range(end//block_size):
-            sum_n.append(np.moveaxis(dset[data_type][start+(i)*block_size:start+(i+1)*block_size], 3, 0)[band].size)
-        sum_n.append(np.moveaxis(dset[data_type][start+(i+1)*block_size:end], 3, 0)[band].size)
+                # count records
+                sum_n = list()
+                for i in range(end//block_size):
+                    sum_n.append(np.moveaxis(dset[data_type][start+(i)*block_size:start+(i+1)*block_size], 3, 0)[band].size)
+                sum_n.append(np.moveaxis(dset[data_type][start+(i+1)*block_size:end], 3, 0)[band].size)
 
-        return (sum(sum_sqr)/sum(sum_n))**(1/2)*scaler
+                std_bands.append((sum(sum_sqr)/sum(sum_n))**(1/2)*scaler)
+
+            return std_bands
+
+        else:
+            band = bands-1
+            # sum squared difference
+            sum_sqr = list()
+            for i in range((end-start)//block_size):
+                sum_sqr.append(((dset[data_type][start+(i)*block_size:start+(i+1)*block_size]/scaler - (mean[band]/scaler))**2).astype(np.float64).sum())
+            sum_sqr.append(((dset[data_type][start+(i+1)*block_size:end]/scaler - (mean[band]/scaler))**2).astype(np.float64).sum())
+
+            # count records
+            sum_n = list()
+            for i in range((end-start)//block_size):
+                sum_n.append((dset[data_type][start+(i)*block_size:start+(i+1)*block_size]).size)
+            sum_n.append((dset[data_type][start+(i+1)*block_size:end]).size)
+
+            return [(sum(sum_sqr)/sum(sum_n))**(1/2)*scaler]